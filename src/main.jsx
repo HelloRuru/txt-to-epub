e5a3eb639a484ad4c46fc547e217c98e7ea47f7c
HelloRuru <vmpvmp1017@gmail.com>
2026-01-29 11:58:33 +0800

    fix: handle SPA redirect from 404.html

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,13 @@ import { ThemeProvider } from './contexts/ThemeContext'
 import App from './App'
 import './index.css'
 
+// Handle GitHub Pages SPA redirect
+const params = new URLSearchParams(window.location.search)
+const redirectPath = params.get('p')
+if (redirectPath) {
+  window.history.replaceState(null, '', decodeURIComponent(redirectPath))
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
